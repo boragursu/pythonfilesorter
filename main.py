434ff6286ad9de4cc613a99a397bf0e026c5bb48
Eren Erol <>
2021-04-14 17:51:14 +0000

    Now the code looks for subject names and create directories for every subject

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import os
 import re
 import shutil
-
-pattern = "cin"
 with open("path.txt") as file:
     FOLDER_PATH = file.read()
-
     filenamelist = os.listdir(FOLDER_PATH)
-for filename in filenamelist:
-    x = re.search(pattern, str(filename))
-    if x:
-        if os.path.exists(FOLDER_PATH+"/"+pattern) == False:
-          try:
-            os.mkdir(FOLDER_PATH+"/"+pattern)
-          except OSError:
-            print ("Creation of the directory %s failed" % FOLDER_PATH)
+
+subjects=["mat","cin","eng","kim","ink"]
+
+for i in subjects:
+  pattern = i
+    
+  for filename in filenamelist:
+      x = re.search(pattern, str(filename))
+      if x:
+          if os.path.exists(FOLDER_PATH+"/"+pattern) == False:
+            try:
+              os.mkdir(FOLDER_PATH+"/"+pattern)
+            except OSError:
+              print ("Creation of the directory %s failed" % FOLDER_PATH)
+            else:
+              print ("Successfully created the directory %s " % FOLDER_PATH)
+            shutil.move(FOLDER_PATH+"/"+filename,FOLDER_PATH+"/"+pattern+"/"+filename)
           else:
-            print ("Successfully created the directory %s " % FOLDER_PATH)
-          shutil.move(FOLDER_PATH+"/"+filename,FOLDER_PATH+"/"+pattern+"/"+filename)
-        else:
-          shutil.move(FOLDER_PATH+"/"+filename,FOLDER_PATH+"/"+pattern)
-          print("Sucesfully moved to existing subject folder")
-    else:
-        pass
+            shutil.move(FOLDER_PATH+"/"+filename,FOLDER_PATH+"/"+pattern)
+            print("Sucesfully moved to existing subject folder")
+      else:
+          pass
