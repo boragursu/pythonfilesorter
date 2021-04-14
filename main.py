857489761cc95f13167d1e00a212882d46ec06b6
Bora Gürsu <>
2021-04-14 17:43:40 +0000

    Added file sorting code now script sorts files according to the pattern variable

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import os
 import re
+import shutil
 
+pattern = "cin"
 with open("path.txt") as file:
     FOLDER_PATH = file.read()
 
     filenamelist = os.listdir(FOLDER_PATH)
 for filename in filenamelist:
-    x = re.search(r"apple", str(filename))
+    x = re.search(pattern, str(filename))
     if x:
-        print(filename)
+        if os.path.exists(FOLDER_PATH+"/"+pattern) == False:
+          try:
+            os.mkdir(FOLDER_PATH+"/"+pattern)
+          except OSError:
+            print ("Creation of the directory %s failed" % FOLDER_PATH)
+          else:
+            print ("Successfully created the directory %s " % FOLDER_PATH)
+          shutil.move(FOLDER_PATH+"/"+filename,FOLDER_PATH+"/"+pattern+"/"+filename)
+        else:
+          shutil.move(FOLDER_PATH+"/"+filename,FOLDER_PATH+"/"+pattern)
+          print("Sucesfully moved to existing subject folder")
     else:
         pass
