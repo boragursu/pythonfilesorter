0d972d9302a3d68cbbfe3ef6d3e85a83c93becf2
boragursu <boragursu@gmail.com>
2021-04-15 00:08:55 +0300

    cleaned code

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import os
 import re
 import shutil
+
 with open("path.txt") as file: #Remember to update path from path.txt!!!
     FOLDER_PATH = file.read()
     filenamelist = os.listdir(FOLDER_PATH)
 
 subjects=["mat","cin","eng","kim","ink","tur","fiz"] #Add subject names here
 
-for i in subjects:
-  pattern = i
+for pattern in subjects:
     
   for filename in filenamelist:
       x = re.search(pattern, str(filename))
       if x:
-          if os.path.exists(FOLDER_PATH+"/"+pattern) == False:
+          if not os.path.exists(f'{FOLDER_PATH}/{pattern}'):
             try:
               os.mkdir(FOLDER_PATH+"/"+pattern)
             except OSError:
@@ -24,5 +24,3 @@ for i in subjects:
           else:
             shutil.move(FOLDER_PATH+"/"+filename,FOLDER_PATH+"/"+pattern)
             print("Sucesfully moved to existing subject folder")
-      else:
-          pass
