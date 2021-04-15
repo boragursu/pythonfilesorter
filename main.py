dc40dc4528b82b359e661e7133537c3cb4cc19b3
Eren Erol <60065768+Eroyun@users.noreply.github.com>
2021-04-15 13:56:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 import os
 import re
 import shutil
-
 with open("path.txt") as file: #Remember to update path from path.txt!!!
     FOLDER_PATH = file.read()
     filenamelist = os.listdir(FOLDER_PATH)
 
-subjects=["mat","cin","eng","kim","ink","tur","fiz"] #Add subject names here
+subjects={} 
+subjectinput=str(input("Please write the name of the class: "))
+patterninput=str(input("Please write file name pattern: "))
+if subjectinput not in subjects.keys():
+  subjects[subjectinput]=[patterninput]
+else:
+  subjects[subjectinput].append(patterninput)
 
-for pattern in subjects:
-    
-  for filename in filenamelist:
-      x = re.search(pattern, str(filename))
-      if x:
-          if not os.path.exists(f'{FOLDER_PATH}/{pattern}'):
-            try:
-              os.mkdir(FOLDER_PATH+"/"+pattern)
-            except OSError:
-              print ("Creation of the directory %s failed" % FOLDER_PATH)
-            else:
-              print ("Successfully created the directory %s " % FOLDER_PATH)
-            shutil.move(FOLDER_PATH+"/"+filename,FOLDER_PATH+"/"+pattern+"/"+filename)
+for x in subjects:
+   for i in subjects[x]:
+      pattern=i
+      for filename in filenamelist:
+          x = re.search(pattern, str(filename))
+          if x:
+              if os.path.exists(FOLDER_PATH+"/"+subjectinput) == False:
+                try:
+                  os.mkdir(FOLDER_PATH+"/"+subjectinput)
+                except OSError:
+                  print ("Creation of the directory %s failed" % FOLDER_PATH)
+                else:
+                  print ("Successfully created the directory %s " % FOLDER_PATH)
+                shutil.move(FOLDER_PATH+"/"+filename,FOLDER_PATH+"/"+subjectinput+"/"+filename)
+              else:
+                shutil.move(FOLDER_PATH+"/"+filename,FOLDER_PATH+"/"+subjectinput)
+                print("Sucesfully moved to existing subject folder")
           else:
-            shutil.move(FOLDER_PATH+"/"+filename,FOLDER_PATH+"/"+pattern)
-            print("Sucesfully moved to existing subject folder")
+              pass
