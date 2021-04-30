3352296dc90494d7f8fef6d5b0da883a583bf6c0
Eren Erol <60065768+Eroyun@users.noreply.github.com>
2021-04-30 22:18:03 +0300

    Create main.py
    Now that the subjects and patterns are being stored inside a JSON file, the files will also be sorted according to the  previously added subjects and patterns.

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 import os
 import re
 import shutil
+import json
+
 with open("path.txt") as file: #Remember to update path from path.txt!!!
     FOLDER_PATH = file.read()
     filenamelist = os.listdir(FOLDER_PATH)
 
-subjects={} 
+
 subjectinput=str(input("Please write the name of the class: "))
 patterninput=str(input("Please write file name pattern: "))
-if subjectinput not in subjects.keys():
-  subjects[subjectinput]=[patterninput]
-else:
-  subjects[subjectinput].append(patterninput)
+if subjectinput!="" and patterninput!="":
+  with open('subjectFile.json') as subjectFile:
+    data=json.load(subjectFile)
 
-for j in subjects:
-   for i in subjects[j]:
-      pattern=i
-      for filename in filenamelist:
-          x = re.search(pattern, str(filename))
-          if x:
-              if os.path.exists(FOLDER_PATH+"/"+j) == False:
-                try:
-                  os.mkdir(FOLDER_PATH+"/"+j)
-                except OSError:
-                  print ("Creation of the directory %s failed" % FOLDER_PATH)
+  if subjectinput not in data.keys():
+    data[subjectinput]=[patterninput]
+  else:
+    data[subjectinput].append(patterninput)  
+
+  with open('subjectFile.json', 'w') as subjectFile:
+    json.dump(data,subjectFile)
+  
+with open('subjectFile.json') as subjectFile:
+  data=json.load(subjectFile)
+  for j in data:
+    for i in data[j]:
+        pattern=i
+        for filename in filenamelist:
+            x = re.search(pattern, str(filename))
+            if x:
+                if os.path.exists(FOLDER_PATH+"/"+j) == False:
+                  try:
+                    os.mkdir(FOLDER_PATH+"/"+j)
+                  except OSError:
+                    print ("Creation of the directory %s failed" % FOLDER_PATH)
+                  else:
+                    print ("Successfully created the directory %s " % FOLDER_PATH)
+                  shutil.move(FOLDER_PATH+"/"+filename,FOLDER_PATH+"/"+j+"/"+filename)
                 else:
-                  print ("Successfully created the directory %s " % FOLDER_PATH)
-                shutil.move(FOLDER_PATH+"/"+filename,FOLDER_PATH+"/"+j+"/"+filename)
-              else:
-                shutil.move(FOLDER_PATH+"/"+filename,FOLDER_PATH+"/"+j)
-                print("Sucesfully moved to existing subject folder")
-          else:
-              pass 
+                  shutil.move(FOLDER_PATH+"/"+filename,FOLDER_PATH+"/"+j)
+                  print("Sucesfully moved to existing subject folder")
+            else:
+                pass 
